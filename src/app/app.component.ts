73c46e58cda81596cff468621adbbb5eeb5256b4
Rafał Czarnecki <s16281@pjwstk.edu.pl>
2019-06-13 15:19:58 +0200

    Before changing the navbar (just bootstrap things)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core'; // służy do pobierania informacji z komponentu - parent'a
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quiz BIU';
+
+  private buttons = {
+   btnRegister : 'active',
+   btnTop : 'link',
+   btnPeople : 'link',
+   btnMessages :'link',
+  }
+
+  addClassActiveOrRemove(name:string){
+    console.log("IMIE ELEMENTU: "+ name);
+  }
+
+
+
 }
