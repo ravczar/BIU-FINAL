1d70cc45670be117c3253ae87e407379d50b57e6
Rafał Czarnecki <s16281@pjwstk.edu.pl>
2019-06-13 16:28:37 +0200

    fixing front page look

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { Input } from '@angular/core'; // służy do pobierania informacji z komponentu - parent'a
+import { Input, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
+
   title = 'Quiz BIU';
+  someclass= 'active';
+
 
   private buttons = {
-   btnRegister : 'active',
-   btnTop : 'link',
+   btnRegister : 'link',
+   btnTop : 'active',
    btnPeople : 'link',
    btnMessages :'link',
+  };
+
+  clearAllToLinkClass (){
+    this.buttons.btnMessages = 'link';
+    this.buttons.btnPeople = 'link';
+    this.buttons.btnRegister = 'link';
+    this.buttons.btnTop = 'link';
   }
 
   addClassActiveOrRemove(name:string){
-    console.log("IMIE ELEMENTU: "+ name);
+    let number: Number;
+    if(name === 'btnRegister') number = 0;
+    else if(name === 'btnTop') number = 1; 
+    else if(name === 'btnPeople') number = 2; 
+    else if(name === 'btnMessages') number = 3; 
+
+    if(this.buttons[`${name}`] == 'link'){
+      this.clearAllToLinkClass ();
+      this.buttons[`${name}`] = 'active';
+    }
+    else if( this.buttons[`${name}`] == 'active')
+      this.buttons[`${name}`] = 'link'
+    //console.log("IMIE ELEMENTU: "+ name);
+    //console.log("WYBIERAMY: " + this.buttons[`${name}`]);
   }
 
 
